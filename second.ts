732a5ab8877f2ec50a9d7262985a8e30ffcd5dad
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-19 00:48:58 +0530

    Destructure dict in param

--- second.ts
+++ second.ts
@@ -1,5 +1,5 @@
-function greet(options:{greeting:string, name:string}):string{
-    return "${options.greeting} ${options.name}"
+function greet({greeting, name}:{greeting:string, name:string}):string{
+    return "${greeting} ${name}"
 }
 
 const message:string = greet({"greeting":"Hello", "name":"Jibin"})
