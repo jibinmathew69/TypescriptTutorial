d2d48ee064b1b8a9a53b9224553f1f248d22bc82
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-20 16:51:03 +0530

    Add Models for Decorator patter

---
+++ decorator.ts
@@ -0,0 +1,15 @@
+class ModelS{
+    public description = "Model S"
+
+    public cost(): number{
+        return 73_000
+    }
+}
+
+class ModelX{
+    public description = "Model X"
+
+    public cost(): number{
+        return 77_000
+    }
+}
