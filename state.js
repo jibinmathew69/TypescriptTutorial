afd9c1679315e4ad7fe88dda39a96530913b7f9f
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 13:27:50 +0530

    Completed state pater

--- state.js
+++ state.js
@@ -1,123 +1,86 @@
-interface State{
-    order: Order
-    
-    cancelOrder(): void
-    verifyPayment(): void
-    shipOrder(): void
-
-}
-
-class Order{
-    public cancelledOrderState: State;
-    public paymentPendingState: State;
-    public orderShipedState: State;
-    public orderBeingPrepared: State;
-
-    public currentState: State;
-
-    constructor() {
+var Order = /** @class */ (function () {
+    function Order() {
         this.cancelledOrderState = new CancelledOrderState(this);
         this.paymentPendingState = new PaymentPendingState(this);
         this.orderShipedState = new OrderShippedState(this);
         this.orderBeingPrepared = new OrderBeingPrepared(this);
-
         this.setState(this.paymentPendingState);
     }
-
-    public setState(state: State) {
+    Order.prototype.setState = function (state) {
         this.currentState = state;
-    }
-
-    public getCurrentState(): State{
+    };
+    Order.prototype.getCurrentState = function () {
         return this.currentState;
-    }
-}   
-
-class CancelledOrderState implements State {
-    
-    order: Order;
-
-    constructor(order: Order) {
+    };
+    return Order;
+}());
+var CancelledOrderState = /** @class */ (function () {
+    function CancelledOrderState(order) {
         this.order = order;
     }
-
-    public cancelOrder() {
+    CancelledOrderState.prototype.cancelOrder = function () {
         console.log('This order is already cancelled.');
         this.order.setState(this.order.cancelledOrderState);
-    }
-
-    public verifyPayment() {
+    };
+    CancelledOrderState.prototype.verifyPayment = function () {
         console.log('The order is cancelled, you cannot pay anymore.');
-    }
-
-    public shipOrder() {
+    };
+    CancelledOrderState.prototype.shipOrder = function () {
         console.log('The order is cancelled, you cannot ship it anymore.');
-    }
-}
-
-class PaymentPendingState implements State {
-    order: Order;
-
-    constructor(order: Order) {
+    };
+    return CancelledOrderState;
+}());
+var PaymentPendingState = /** @class */ (function () {
+    function PaymentPendingState(order) {
         this.order = order;
     }
-
-    cancelOrder() {
+    PaymentPendingState.prototype.cancelOrder = function () {
         console.log('Cancelling your unpaid order...');
         this.order.setState(this.order.cancelledOrderState);
-    }
-
-    verifyPayment() {
+    };
+    PaymentPendingState.prototype.verifyPayment = function () {
         console.log('Payment verified! Shipping soon.');
         this.order.setState(this.order.orderBeingPrepared);
-    }
-    shipOrder() {
+    };
+    PaymentPendingState.prototype.shipOrder = function () {
         console.log('Cannot ship order when payment is pending.');
-    }
-}
-
-class OrderBeingPrepared implements State {
-    order: Order;
-
-    constructor(order: Order) {
+    };
+    return PaymentPendingState;
+}());
+var OrderBeingPrepared = /** @class */ (function () {
+    function OrderBeingPrepared(order) {
         this.order = order;
     }
-
-    cancelOrder() {
+    OrderBeingPrepared.prototype.cancelOrder = function () {
         console.log('Cancelling your order... You will be refunded.');
         this.order.setState(this.order.cancelledOrderState);
-    }
-    verifyPayment() {
+    };
+    OrderBeingPrepared.prototype.verifyPayment = function () {
         console.log('Payment is already verified.');
-    }
-    shipOrder() {
+    };
+    OrderBeingPrepared.prototype.shipOrder = function () {
         console.log('Shipping your order now...');
         this.order.setState(this.order.orderShipedState);
-    }
-}
-
-class OrderShippedState implements State {
-    order: Order;
-
-    constructor(order: Order) {
+    };
+    return OrderBeingPrepared;
+}());
+var OrderShippedState = /** @class */ (function () {
+    function OrderShippedState(order) {
         this.order = order;
     }
-
-    cancelOrder() {
+    OrderShippedState.prototype.cancelOrder = function () {
         console.log('You cannot cancel an order that has been shipped.');
-    }
-    verifyPayment() {
+    };
+    OrderShippedState.prototype.verifyPayment = function () {
         console.log('Payment is already verified.');
-    }
-    shipOrder() {
+    };
+    OrderShippedState.prototype.shipOrder = function () {
         console.log('Order is already shipped.');
-    }
-}
-
-let order = new Order();
-
+    };
+    return OrderShippedState;
+}());
+var order = new Order();
 order.getCurrentState().verifyPayment();
 order.getCurrentState().shipOrder();
 order.getCurrentState().cancelOrder();
-
-console.log('Order state: ' + (<any> order.getCurrentState()).constructor.name);
+console.log('Order state: ' + order.getCurrentState().constructor.name);
