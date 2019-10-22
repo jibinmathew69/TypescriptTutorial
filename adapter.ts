184ee824229cd0003f3f06472a2c33b9c5c61aac
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-22 12:41:11 +0530

    Add adapter pattern classes

---
+++ adapter.ts
@@ -0,0 +1,19 @@
+interface iphone{
+
+}
+
+interface android{
+    
+}
+
+class iphone{
+    public useLighting(){
+        console.log("lighting charger")
+    }
+}
+
+class pixel{
+    public useUSB(){
+        console.log("USB charger")
+    }
+}
